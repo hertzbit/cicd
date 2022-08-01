1bfbc2a56b21c08ccb5f5e703f533d33badcca41
iamchetankrishna <84701931+prochetankrishna@users.noreply.github.com>
2022-08-01 13:31:10 +0530

    Adds Logger

--- src/main/java/com/herzbit/cicd/controller/GreetingController.java
+++ src/main/java/com/herzbit/cicd/controller/GreetingController.java
@@ -1,5 +1,7 @@
 package com.herzbit.cicd.controller;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,8 +12,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/greeting")
 public class GreetingController {
 
+    private static final Logger LOGGER = LogManager.getLogger(GreetingController.class);
+    
     @GetMapping
     public ResponseEntity<String> greetUser() {
+        LOGGER.info("Hello World !");
         return new ResponseEntity<>("Hello, Welcome to CICD Demo", HttpStatus.OK);
     }
 }
