7e71ea5ad4016069ae1e122aa10d8e8b93616573
iamchetankrishna <84701931+prochetankrishna@users.noreply.github.com>
2022-08-01 10:37:31 +0530

    Adds JUnit Test Case

--- src/test/java/com/herzbit/cicd/CicdApplicationTests.java
+++ src/test/java/com/herzbit/cicd/CicdApplicationTests.java
@@ -2,12 +2,14 @@ package com.herzbit.cicd;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class CicdApplicationTests {
 
 	@Test
 	void contextLoads() {
-	}
 
+		assertTrue(true);
+	}
 }
